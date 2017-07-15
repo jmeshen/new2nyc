91e53060af1173714aad20127f166d7e5527c283
kmasila00 <kmasila00@gmail.com>
2017-07-15 04:51:03 -0400

    added prelim data

---
+++ data.js
@@ -0,0 +1,84 @@
+const countries = {
+  dominicanRepublic: {
+    title: 'Dominican Republic',
+    consulate: {
+      lat: 40.757253,
+      long: -73.986768
+    },
+    addlPlaces: []
+  },
+  china: {
+    title: 'China',
+    consulate: {
+      lat: 40.762193,
+      long: -74.000577
+    },
+    addlPlaces: []
+  },
+  mexico: {
+    title: 'Mexico',
+    consulate: {
+      lat: 40.750821,
+      long: -73.979851
+    },
+    addlPlaces: []
+  },
+  jamaica: {
+    title: 'Jamaica',
+    consulate: {
+      lat: 40.754248,
+      long: -73.971434
+    },
+    addlPlaces: []
+  },
+  guyana: {
+    title: 'Guyana',
+    consulate: {
+      lat: 40.755023,
+      long:-73.992850
+    },
+    addlPlaces: []
+  },
+  ecuador: {
+    title: 'Ecuador',
+    consulate: {
+      lat: 40.746451,
+      long: -73.897262
+    },
+    addlPlaces: []
+  },
+  haiti: {
+    title: 'Haiti',
+    consulate: {
+      lat: 40.750848,
+      long: -73.971874
+    },
+    addlPlaces: []
+  },
+  trinidad: {
+    title: 'Trindad & Tobago',
+    consulate: {
+      lat: 40.706609,
+      long: -74.006250
+    },
+    addlPlaces: []
+  },
+  india: {
+    title: 'India',
+    consulate: {
+      lat: 40.767314,
+      long: -73.970006
+    },
+    addlPlaces: []
+  },
+  russia: {
+    title: 'Russia',
+    consulate: {
+      lat: 40.784511,
+      long: -73.957008
+    },
+    addlPlaces: []
+  },
+}
+
+export default countries;
